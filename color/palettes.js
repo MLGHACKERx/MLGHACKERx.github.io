e99823fa055b86e90539f08fda3872d980c9b7ad
MLG <mlghackerx@gmail.com>
2022-05-04 11:07:25 -0400

    update

--- color/palettes.js
+++ color/palettes.js
@@ -1,3 +1,11 @@
 function copyColor(color) {
 	navigator.clipboard.writeText(color);
-}
+};
+
+array = document.getElementsByClassName("palette-block");
+
+Object.keys(array).forEach(function (key){
+    childs = array[key].children;
+    childElement = childs[0];
+    array[key].style.background = childElement.innerHTML;
+});
